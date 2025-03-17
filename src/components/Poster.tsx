cf530e297a85a6809e5d44a18982b2b87aa1e6ba
Bimai6 <bimaivfx@gmail.com>
2025-03-17 21:27:22 +0100

    Fix poster styles

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -4,19 +4,19 @@ import { ImageLinks } from "../utils/ImageLinks";
 const Poster = () => {
     return (
         <div className="flex flex-col bg-white h-screen w-1/3 overflow-y-hidden">
-            <div className="text-purple-800 font-extrabold h-full w-full">
-                <div className="text-8xl pl-3">
+            <div className="flex flex-col text-purple-800 font-extrabold h-full w-full">
+                <div className="flex flex-col text-8xl pl-3">
                     <p>MARIO</p>
                     <p>LEBRERO</p>
                     <p>GARCIA</p>
                 </div>
-                <div className="pr-3 flex flex-col place-items-end mt-25 text-5xl">
+                <div className="pr-3 flex flex-col place-items-end text-5xl mt-auto mb-5">
                     <p>WEB</p>
                     <p>DEVELOPER</p>
                 </div>
             </div>
-            <div className="bg-gray-500 w-full flex-1 flex items-center justify-center">
-                <img className="h-full m-auto w-4/6"
+            <div className="bg-gray-500 w-full min-h-[50vh] flex-1 flex items-center justify-center p-4">
+                <img className="h-auto w-4/6"
                 src={ImageLinks.selfie} alt="Mario Lebrero's image"/>
             </div>
         </div>
