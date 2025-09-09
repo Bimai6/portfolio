834a9356970384fcaa01f8dea321a8b2b88a445d
Bimai6 <bimaivfx@gmail.com>
2025-09-09 17:03:25 +0200

    refactor: remove unused utility

--- src/utils/FixedFields.tsx
+++
@@ -1,9 +0,0 @@
-export function handleTitle(name: string) {
-    return name === 'magnolia-hotels' ? 'MAGNOLIA HOTELS' : name === 'EyeOfTheDoom' ? 'EYE OF THE DOOM' : name === 'mugen-the-seinen-manga-gate' ? 'MUGEN' : name === 'theuss' ? 'THEUSS' : undefined;
-}
-
-export function handleDate(date: string) {
-    const dateArray = date.split('T');
-    const datePieces = dateArray[0].split('-');
-    return datePieces[2] + '.' + datePieces[1] + '.' + datePieces[0].substring(2,4);
-}
