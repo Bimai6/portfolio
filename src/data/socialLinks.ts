d45a512fd83f47caf316e1e1d3e35c50a0e41b72
Bimai6 <bimaivfx@gmail.com>
2025-09-09 16:50:22 +0200

    feat: add SocialLink model

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,6 +1,6 @@
-import { RSSLink } from "../models/RSSLink";
+import { SocialLink } from "../models/SocialLink";
 
-export const socialLinks: RSSLink[] = [
-    new RSSLink("LinkedIn Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/gh-white_q5jbqn.png", "https://www.linkedin.com/in/mario-lebrero-garcia/"),
-    new RSSLink("GitHub Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/li-white_ogkbhy.png", "https://github.com/Bimai6")
+export const socialLinks: SocialLink[] = [
+    new SocialLink("LinkedIn Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/gh-white_q5jbqn.png", "https://www.linkedin.com/in/mario-lebrero-garcia/"),
+    new SocialLink("GitHub Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/li-white_ogkbhy.png", "https://github.com/Bimai6")
 ];
---
+++ src/models/SocialLink.ts
@@ -0,0 +1,7 @@
+export class SocialLink {
+    constructor(
+        public alt: string,  
+        public image: string,
+        public url: string,  
+    ) {}
+}
