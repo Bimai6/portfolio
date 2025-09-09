698b14d5c07130b0cd6fb04d2acf5708177ffb93
Bimai6 <bimaivfx@gmail.com>
2025-09-09 16:47:22 +0200

    feat: add socialLinks to data folder to centralize social networks links

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,6 @@
+import { RSSLink } from "../models/RSSLink";
+
+export const socialLinks: RSSLink[] = [
+    new RSSLink("LinkedIn Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/gh-white_q5jbqn.png", "https://www.linkedin.com/in/mario-lebrero-garcia/"),
+    new RSSLink("GitHub Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/li-white_ogkbhy.png", "https://github.com/Bimai6")
+];
