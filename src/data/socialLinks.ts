5d10322d84a3794346f4c54388208863e94bb3f0
Bimai6 <bimaivfx@gmail.com>
2025-09-15 16:54:07 +0200

    refactor: change the order of socialLinks appeareance

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,6 +1,6 @@
 import { SocialLink } from "../models/SocialLink";
 
 export const socialLinks: SocialLink[] = [
-    new SocialLink("LinkedIn Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/gh-white_q5jbqn.png", "https://www.linkedin.com/in/mario-lebrero-garcia/"),
-    new SocialLink("GitHub Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/li-white_ogkbhy.png", "https://github.com/Bimai6")
+    new SocialLink("GitHub Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/gh-white_q5jbqn.png", "https://github.com/Bimai6"),
+    new SocialLink("LinkedIn Logo", "https://res.cloudinary.com/demqnwfff/image/upload/v1741289209/li-white_ogkbhy.png", "https://www.linkedin.com/in/mario-lebrero-garcia/")
 ];
