78e2f88089a83267b4e6259e1d1763504be2fcc6
Bimai6 <bimaivfx@gmail.com>
2025-09-17 22:15:04 +0200

    styles: change on Poster component info display

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -5,20 +5,18 @@ const Poster = () => {
     return (
         <div className="flex flex-col bg-white h-screen w-1/3 overflow-y-hidden">
             <div className="flex flex-col text-purple-800 font-extrabold h-full w-full">
-                <div className="name_text flex flex-col text-8xl px-3 mr-3">
+                <div className="name_text flex flex-col text-8xl mt-5 mx-5 mb-auto">
                     <p>MARIO</p>
                     <p>LEBRERO</p>
-                    <p>GARCIA</p>
                 </div>
-                <div className="statement_text pr-3 flex flex-col place-items-end text-5xl mt-auto mb-5">
+                <img className="self-center h-auto w-4/6"
+                src="https://res.cloudinary.com/demqnwfff/image/upload/v1741285597/mepurple_ljpwvb.png" alt="Mario Lebrero's portrait"/>
+                <div className="statement_text mx-5 flex flex-col place-items-end text-5xl mt-auto mb-7">
                     <p>WEB</p>
                     <p>DEVELOPER</p>
                 </div>
             </div>
-            <div className="bg-gray-500 w-full min-h-[48vh] flex-1 flex items-center justify-center p-4">
-                <img className="h-auto w-4/6"
-                src="https://res.cloudinary.com/demqnwfff/image/upload/v1741285597/mepurple_ljpwvb.png" alt="Mario Lebrero's portrait"/>
-            </div>
+            
         </div>
     )
 }
