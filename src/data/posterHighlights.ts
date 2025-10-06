da56473135d896f8ca30e3ef47ba17000a38bb8d
Bimai6 <bimaivfx@gmail.com>
2025-10-06 20:04:16 +0200

    feat: add posterHighlight model and data //TODO: change BackPoster component

---
+++ src/data/posterHighlights.ts
@@ -0,0 +1,7 @@
+import { PosterHighlight } from "@/models/PosterHighlight";
+
+export const posterHighlights : PosterHighlight[] = [
+    new PosterHighlight("Web developer, passionate about programming, creating beautiful UI, eager to grow my skills and keep learning every day.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759767369/IMG-20250130-WA0026_ws00f8.jpg", "Success is easy to achieve. The difficult thing is to deserve it. - Albert Camus", "Mario concentrated in his office room"),
+    new PosterHighlight("My setup, eager to have a second screen", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759767369/IMG-20250130-WA0026_ws00f8.jpg", "Why did I buy an american keyboard? I miss 'ñ' char - Me", "Mario's setup"),
+    new PosterHighlight("When I'm not coding, you'll find me on poetry slams expressing emotions through verses.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759767369/IMG-20250130-WA0026_ws00f8.jpg", "Your chest, the elytra that hides the wing of a cherub - Me", "Mario participating on a poetry slam"),
+]
---
+++ src/models/PosterHighlight.ts
@@ -0,0 +1,8 @@
+export class PosterHighlight {
+    constructor(
+        public text: string,
+        public img: string,
+        public quote: string,
+        public alt: string
+    ){}
+}
