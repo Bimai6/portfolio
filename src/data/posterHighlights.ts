e4793494f1f32c4c05491e6dd95d6fea6619a821
Bimai6 <bimaivfx@gmail.com>
2025-10-08 21:52:10 +0200

    chore: change url for optimized images on posterHighlights data

--- src/data/posterHighlights.ts
+++ src/data/posterHighlights.ts
@@ -1,7 +1,7 @@
 import { PosterHighlight } from "@/models/PosterHighlight";
 
 export const posterHighlights : PosterHighlight[] = [
-    new PosterHighlight("Driven by code and design, I love creating elegant interfaces and learning every day.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759949092/meonsetup_zriln3.png", `"Success is easy to achieve. The difficult thing is to deserve it."`, "- Albert Camus", "Mario concentrated in his office room"),
-    new PosterHighlight("Enjoying my workspace, dreaming of that second screen to boost creativity.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759949100/setup_fuzuvr.png", `"Why did I buy an american keyboard? I miss 'ñ' char"`, "- Me", "Mario's setup"),
-    new PosterHighlight("When I'm not coding, you'll find me on poetry slams expressing emotions through verses.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759778085/IMG-20250130-WA0026_ws00f8.png", `"Your chest, the elytra that hides the wing of a cherub"`, "- Me", "Mario participating on a poetry slam"),
+    new PosterHighlight("Driven by code and design, I love creating elegant interfaces and learning every day.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759953010/meonsetup_zriln3.webp", `"Success is easy to achieve. The difficult thing is to deserve it."`, "- Albert Camus", "Mario concentrated in his office room"),
+    new PosterHighlight("Enjoying my workspace, dreaming of that second screen to boost creativity.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759953020/setup_fuzuvr.webp", `"Why did I buy an american keyboard? I miss 'ñ' char"`, "- Me", "Mario's setup"),
+    new PosterHighlight("When I'm not coding, you'll find me on poetry slams expressing emotions through verses.", "https://res.cloudinary.com/dus7e3jkj/image/upload/v1759952996/IMG-20250130-WA0026_ws00f8.webp", `"Your chest, the elytra that hides the wing of a cherub"`, "- Me", "Mario participating on a poetry slam"),
 ]
